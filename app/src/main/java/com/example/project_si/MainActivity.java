a516c2c46dcd4f7b51a1177fb3dcb0b758620063
Dragos2099 <dragos.tudosoiu@student.upt.ro>
2021-05-12 17:36:48 +0300

    Temp display

---
+++ app/src/main/java/com/example/project_si/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.project_si;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView a,b,c,d;
+    Button btn;
+    DatabaseReference reff;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        a=(TextView)findViewById(R.id.tempview);
+       // btn=(Button)findViewById(R.id.btn);
+
+        reff= FirebaseDatabase.getInstance().getReference();
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String temp=dataSnapshot.child("Temperatura").getValue().toString();
+                a.setText(temp);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        /*
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reff= FirebaseDatabase.getInstance().getReference();
+                reff.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String temp=dataSnapshot.child("Temperatura").getValue().toString();
+                        a.setText(temp);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+*/
+
+    }
+}
