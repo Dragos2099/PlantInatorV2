29c65e9648807c3d1a825ffe2f21944cc6f1bb39
sonia <sonia.deca@student.upt.ro>
2021-05-14 01:34:57 +0300

    Plant3 Title + Back Button

--- app/src/main/java/com/example/project_si/Plant3Activity.java
+++ app/src/main/java/com/example/project_si/Plant3Activity.java
@@ -2,13 +2,27 @@ package com.example.project_si;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Plant3Activity extends AppCompatActivity {
+
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plant3);
+
+        back= findViewById(R.id.backToProfiles3);
+        back.setOnClickListener(v -> backToProfiles());
+
     }
+
+    public void backToProfiles(){
+        Intent intent = new Intent(this,ProfilesActivity.class);
+        startActivity(intent);
+    }
+
 }
