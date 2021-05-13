d40c141d7650e7dabdb15c0db150f6785f73eedb
sonia <sonia.deca@student.upt.ro>
2021-05-14 00:24:07 +0300

    Profiles Page + Back Button

---
+++ app/src/main/java/com/example/project_si/ProfilesActivity.java
@@ -0,0 +1,30 @@
+package com.example.project_si;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ProfilesActivity extends AppCompatActivity {
+
+    private Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profiles);
+
+        back= findViewById(R.id.back);
+        back.setOnClickListener(v -> backToMainActivity());
+
+    }
+
+
+    public void backToMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+}
