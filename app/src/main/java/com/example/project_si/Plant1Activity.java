ebbbea23bc09e7d4c36445795e3f6b8554548673
sonia <sonia.deca@student.upt.ro>
2021-05-15 02:53:01 +0300

    Plant 1 Choice Box

--- app/src/main/java/com/example/project_si/Plant1Activity.java
+++ app/src/main/java/com/example/project_si/Plant1Activity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Plant1Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plant1Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Button back;
 
@@ -18,6 +26,22 @@ public class Plant1Activity extends AppCompatActivity {
         back= findViewById(R.id.backToProfiles1);
         back.setOnClickListener(v -> backToProfiles());
 
+        Spinner spinner = findViewById(R.id.spinner1);
+
+
+        List<String> categories = new ArrayList<>();
+
+        categories.add(0,"Choose your plant here");
+        categories.add("Cactus");
+        categories.add("Orchid");
+        categories.add("Carnivorous Plant");
+
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,categories);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     public void backToProfiles(){
@@ -25,4 +49,20 @@ public class Plant1Activity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        if(parent.getItemAtPosition(position).equals("Choose your plant here")){
+            //nothing
+        }
+        else{
+
+            String text = parent.getItemAtPosition(position).toString();
+            Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    }
 }
