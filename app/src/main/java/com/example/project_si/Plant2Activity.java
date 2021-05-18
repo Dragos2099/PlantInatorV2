1802d36f8a023e822c126c47efadb51ef384f9a2
sonia <sonia.deca@student.upt.ro>
2021-05-18 03:21:13 +0300

    Send Plant2 Type to Firebase

--- app/src/main/java/com/example/project_si/Plant2Activity.java
+++ app/src/main/java/com/example/project_si/Plant2Activity.java
@@ -11,12 +11,26 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Plant2Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Button back;
+
+    private Button saveButton;
+
+    private DatabaseReference databaseReference;
+
+    private String choice;
+
+    private Spinner spinner;
+
+    Member member;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +40,7 @@ public class Plant2Activity extends AppCompatActivity implements AdapterView.OnI
         back= findViewById(R.id.backToProfiles2);
         back.setOnClickListener(v -> backToProfiles());
 
-        Spinner spinner = findViewById(R.id.spinner2);
+        spinner = findViewById(R.id.spinner2);
 
 
         List<String> categories2 = new ArrayList<>();
@@ -36,12 +50,22 @@ public class Plant2Activity extends AppCompatActivity implements AdapterView.OnI
         categories2.add("Orchid");
         categories2.add("Carnivorous Plant");
 
+        saveButton=findViewById(R.id.saveButton2);
+
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+
+        databaseReference = database.getReference("Plant2_Type");
 
         ArrayAdapter<String> adapter;
         adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,categories2);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+
+        member=new Member();
+
+        saveButton.setOnClickListener(v -> saveValue(choice));
 
     }
 
@@ -61,11 +85,26 @@ public class Plant2Activity extends AppCompatActivity implements AdapterView.OnI
 
             String text = parent.getItemAtPosition(position).toString();
             Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+            choice=spinner.getSelectedItem().toString();
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
     }
+
+
+    void saveValue(String choice){
+        if(choice.equals("Choose your plant here")){
+            Toast.makeText(this,"Please select a plant type",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else{
+            member.setPlantType(choice);
+            databaseReference.setValue(choice);
+            Toast.makeText(this,"Value saved !",Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
 
